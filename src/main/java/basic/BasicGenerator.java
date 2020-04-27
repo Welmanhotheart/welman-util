e7fa74bed59646bb0ab9572adde423dcd4bbf77c
Welmanhotheart <a274936580@163.com>
2020-04-27 20:54:49 +0800

    工具类代码提交

--- src/main/java/basic/BasicGenerator.java
+++ src/main/java/basic/BasicGenerator.java
@@ -3,7 +3,6 @@ package basic;//: net/mindview/util/BasicGenerator.java
 // with a default (no-arg) constructor.
 
 public class BasicGenerator<T> implements Generator<T> {
-
     private Class<T> type;
 
     public BasicGenerator(Class<T> type) {
